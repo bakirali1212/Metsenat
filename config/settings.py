99b58d2ce5ac4e47729583d51351dd7ef3e4d725
Bakirali_Zokirov <160619651+bakirali1212@users.noreply.github.com>
2024-05-11 21:00:31 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
