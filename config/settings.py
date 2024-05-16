55a8bc3dd9c6b5cc441a517ea17dc6769f080c55
Bakirali_Zokirov <160619651+bakirali1212@users.noreply.github.com>
2024-05-16 19:19:48 +0500

    commir

--- config/settings.py
+++ config/settings.py
@@ -31,14 +31,7 @@ INSTALLED_APPS = [
     'drf_yasg',
 ]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
-MEDIA_URL = '/media/'
-
-
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
@@ -127,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "/var/www/static/",
+# ]
+# MEDIA_URL = '/media/'
