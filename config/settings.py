ac6a274b0c98af29d0901c09eea85fed46b9f1ef
Bakirali_Zokirov <160619651+bakirali1212@users.noreply.github.com>
2024-05-16 19:14:54 +0500

    first1

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,15 @@ INSTALLED_APPS = [
     'drf_yasg',
 ]
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+MEDIA_URL = '/media/'
+
+
+# STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
